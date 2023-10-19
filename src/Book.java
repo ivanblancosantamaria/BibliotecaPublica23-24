4c7f87f5c455c55aac112979f42a06ddbb01be34
ivanblancosantamaria <ivan.blanco.movil@gmail.com>
2023-10-19 17:21:00 +0200

    creacion de la clase author con sus atributos y metodos

---
+++ src/Book.java
@@ -0,0 +1,50 @@
+import java.util.Date;
+
+public class Book {
+
+    private String isbn;
+    private String title;
+    private String fullNameAuthor;
+    private String libraryGenre;
+    private Date publicationDate;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFullNameAuthor() {
+        return fullNameAuthor;
+    }
+
+    public void setFullNameAuthor(String fullNameAuthor) {
+        this.fullNameAuthor = fullNameAuthor;
+    }
+
+    public String getLibraryGenre() {
+        return libraryGenre;
+    }
+
+    public void setLibraryGenre(String libraryGenre) {
+        this.libraryGenre = libraryGenre;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+}
