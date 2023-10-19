b595c69ef45e2703da9938d6b5b3bda2bf427712
ivanblancosantamaria <ivan.blanco.movil@gmail.com>
2023-10-19 17:21:00 +0200

    creacion de la clase LibraryGenre con sus atributos y metodos

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello world!");
-    }
-    //biblioteca
-=======
+
 
         System.out.println("Hello world!");
     }
@@ -12,5 +8,5 @@ public class Main {
     
     
     
->>>>>>> 8994ef9 (creacion de la clase author con sus atributos y metodos)
+
 }
